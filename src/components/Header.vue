93ad7319e6ffe079848b80889d34ebee04bb8959
DIOMANDE-CODE <diomandedroh79@gmail.com>
2022-04-26 04:05:01 +0200

    version 2 du site

---
+++ src/components/Header.vue
@@ -0,0 +1,122 @@
+<template>
+  <div>
+    <!-- ======= Header ======= -->
+    <header id="header" class="header fixed-top" data-scrollto-offset="0">
+      <div
+        class="
+          container-fluid
+          d-flex
+          align-items-center
+          justify-content-between
+        "
+      >
+        <a
+          href="index.html"
+          class="logo d-flex align-items-center scrollto me-auto me-lg-0"
+        >
+          <!-- Uncomment the line below if you also wish to use an image logo -->
+          <!-- <img src="../assets/img/logo.png" alt=""> -->
+          <h1>
+            <span style="color: #f15d22"> DIGITAL </span
+            ><span style="color: #0b3146"> + </span><span></span>
+          </h1>
+        </a>
+
+        <nav id="navbar" class="navbar">
+          <ul>
+            <li class="dropdown">
+              <a href="#"><span>Acceuil</span> </a>
+            </li>
+
+            <li>
+              <a class="nav-link scrollto" href="index.html#about"
+                >Qui sommes-nous ?</a
+              >
+            </li>
+            <li>
+              <a class="nav-link scrollto" href="index.html#services"
+                >Nos Services</a
+              >
+            </li>
+            <li>
+              <a class="nav-link scrollto" href="index.html#contact"
+                >Nous joindre</a
+              >
+            </li>
+          </ul>
+          <i class="bi bi-list mobile-nav-toggle d-none"></i>
+        </nav>
+        <!-- .navbar -->
+      </div>
+    </header>
+    <!-- End Header -->
+
+    <!-- Carrousel -->
+    <div
+      id="carouselExampleSlidesOnly"
+      class="carousel slide"
+      data-ride="carousel"
+    >
+      <div class="carousel-inner" >
+        <div class="carousel-item active">
+          <img
+            class="d-block w-100"
+            src="../assets/img/gif_1.gif"
+            alt="First slide"
+            style="height:800px; width:300px;"
+          />
+        </div>
+        <div class="carousel-item">
+          <img
+            class="d-block w-100"
+            src="../assets/img/consultation.png"
+            alt="First slide"
+            style="height:800px; width:300px;"
+          />
+        </div>
+        <div class="carousel-item">
+          <img
+            class="d-block w-100"
+            src="../assets/img/banner2.jpg"
+            alt="First slide"
+            style="height:600px; width:300px;"
+          />
+        </div>
+      </div>
+    </div>
+
+    <!-- End Carrousel -->
+    <section id="hero-animated" class="hero-animated d-flex align-items-center">
+      <div
+        class="
+          container
+          d-flex
+          flex-column
+          justify-content-center
+          align-items-center
+          text-center
+          position-relative
+        "
+        data-aos="zoom-out"
+      >
+        <h2>Bienvenue à <span>DEK DIGITAL +</span></h2>
+        <p>
+          Et voluptate esse accusantium accusamus natus reiciendis quidem
+          voluptates similique aut.
+        </p>
+        <div class="d-flex">
+          <a
+            href="https://www.youtube.com/watch?v=LXb3EKWsInQ"
+            class="glightbox btn-watch-video d-flex align-items-center"
+            ><i class="bi bi-play-circle"></i><span>Voir la vidéo</span></a
+          >
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+<script>
+export default {
+  name: "Header",
+};
+</script>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,59 +1,43 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+
+    <main id="main">
+      <service></service>
+      <about></about>
+      <action></action>
+      <focus></focus>
+      <feature></feature>
+      <produits></produits>
+      <contact></contact>
+      
+    </main>
+    <!-- End #main -->
   </div>
 </template>
 
 <script>
+import service from '../components/sections/services.vue'
+import about from '../components/sections/about.vue'
+import action from '../components/sections/action.vue'
+import focus from '../components/sections/focus.vue'
+import feature from '../components/sections/features.vue'
+import produits from './sections/produits.vue'
+import contact from '../components/sections/contact.vue'
+
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  name: "HelloWorld",
+  components : {
+    contact,
+    service,
+    about,
+    action,
+    focus,
+    feature,
+    produits
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
 </style>
